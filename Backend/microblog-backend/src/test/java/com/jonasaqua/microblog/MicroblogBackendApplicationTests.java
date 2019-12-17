444b14bff02af2f33a360522f1d257879bc5e4a0
Jonas Aqua <me@jonasaqua.com>
2019-12-17 14:35:01 +0100

    Added Backend

---
+++ Backend/microblog-backend/src/test/java/com/jonasaqua/microblog/MicroblogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonasaqua.microblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroblogBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
