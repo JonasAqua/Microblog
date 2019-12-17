5326007939193f80b4c6b58c73b2559e4e3c8ca2
Jonas Aqua <me@jonasaqua.com>
2019-12-17 15:25:14 +0100

    Added EntryController

---
+++ Backend/microblog-backend/src/main/java/com/jonasaqua/microblog/entry/EntryController.java
@@ -0,0 +1,14 @@
+package com.jonasaqua.microblog.entry;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/entry")
+public class EntryController {
+    @GetMapping
+    public String getSampleEntry() {
+        return "Hello, world!";
+    }
+}
