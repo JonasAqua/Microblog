3530592ed5d29aaf600a42a963c4f768c0ad7c4d
Jonas Aqua <me@jonasaqua.com>
2019-12-19 16:07:16 +0100

    Added Test getEntryWithNonexistentIdThrowsEntryNotFoundException

---
+++ Backend/microblog-backend/src/test/java/com/jonasaqua/microblog/entry/service/EntryServiceTest.java
@@ -0,0 +1,40 @@
+package com.jonasaqua.microblog.entry.service;
+
+import com.jonasaqua.microblog.entry.persistence.Entry;
+import com.jonasaqua.microblog.entry.persistence.EntryRepository;
+import com.jonasaqua.microblog.exceptions.EntryNotFoundException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EntryServiceTest {
+    @Mock
+    private EntryRepository entryRepositoryMock;
+
+    private EntryService entryService;
+
+    @Before
+    public void setUp() {
+        this.entryService = new EntryService(entryRepositoryMock);
+    }
+
+    @Test(expected = EntryNotFoundException.class)
+    public void getEntryWithNonexistentIdThrowsEntryNotFoundException() {
+        // given
+        Long id = 42l;
+        given(entryRepositoryMock.findById(id)).willReturn(Optional.empty());
+
+        // when
+        Entry entry = entryService.getEntry(id);
+    }
+}
