c7eeb43eeddb19651bfb53a3f11faf466a06a2b4
Jonas Aqua <accounts@jonasaqua.com>
2019-12-24 22:13:50 +0100

    Basic Structure of Frontend with Homepage and /all

--- Frontend/microblog-frontend/src/app/app.module.ts
+++ Frontend/microblog-frontend/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: PostListComponent },
+      { path: 'all', component: PostListComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
